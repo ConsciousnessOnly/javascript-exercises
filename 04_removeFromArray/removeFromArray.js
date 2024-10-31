e39be46ccdaaaeb86103b9553ce9548b44a42dfd
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-10-31 17:06:54 +0800

    feat(removeFromArray): removes a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, deletionNum) {
+    let returnArray = [];
+    for (let checkPoint = 0; checkPoint < array.length; checkPoint++) {
+        if (array[checkPoint] !== deletionNum) {
+            const additionValue = array[checkPoint];
+            returnArray.push(additionValue);
+        }
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
