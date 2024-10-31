aac360c74cf6baf625dc8cebc15e563bda4e67a1
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-10-31 10:42:49 +0800

    fix: reverses single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const wordLength = word.length;
+    let reversedWord = "";
+    // Reverse word from last char to fist char
+    for (let charOrder = wordLength-1; charOrder >= 0; charOrder--)
+        {
+        reversedWord += word[charOrder];
+        }
+    return reversedWord;
 };
 
 // Do not edit below this line
