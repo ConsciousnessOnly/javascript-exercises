00a28d730ed74028a00fc111b4b8cf9cd8f9985f
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-10-31 19:14:49 +0800

    refactor: use splice method remove number

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,23 @@
 const removeFromArray = function (array, deletionNum) {
-    let returnArray = [];
-    for (let checkPoint = 0; checkPoint < array.length; checkPoint++) {
-        if (array[checkPoint] !== deletionNum) {
-            const additionValue = array[checkPoint];
-            returnArray.push(additionValue);
+    console.log(`Array: [${array}], Remove Number: ${deletionNum} `);
+
+    let checkIndex = 0;
+
+    while (checkIndex < array.length) {
+        console.log(`Check array[${checkIndex}], Array length: ${array.length}`);
+
+        if (array[checkIndex] === deletionNum) {
+            console.log(`Remove array[${checkIndex}] value: ${array[checkIndex]}`);
+            array.splice(checkIndex, 1); // 2nd parameter means remove one item only
+        }
+        else {
+            console.log(`No need to remove; check next.`);
+            checkIndex++;
         }
     }
-    return returnArray;
-};
+    console.log(`Finished, return [${array}].`);
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
