108990d4fca654f8bebc3a94299be88b7228bbf4
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-10-31 19:56:28 +0800

    feat: removes multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,15 @@
-const removeFromArray = function (array, deletionNum) {
-    console.log(`Array: [${array}], Remove Number: ${deletionNum} `);
-
-    let checkIndex = 0;
-
-    while (checkIndex < array.length) {
-        console.log(`Check array[${checkIndex}], Array length: ${array.length}`);
-
-        if (array[checkIndex] === deletionNum) {
-            console.log(`Remove array[${checkIndex}] value: ${array[checkIndex]}`);
-            array.splice(checkIndex, 1); // 2nd parameter means remove one item only
+const removeFromArray = function (array, ...deletionNum) {    
+    for(let number of deletionNum) {
+        let checkIndex = 0;
+        while (checkIndex < array.length) {
+            if (array[checkIndex] === number) {
+                array.splice(checkIndex, 1); // 2nd parameter means remove one item only
+            }
+            else {
+                checkIndex++;
+            }
         }
-        else {
-            console.log(`No need to remove; check next.`);
-            checkIndex++;
-        }
-    }
-    console.log(`Finished, return [${array}].`);
+    }    
     return array;
 }
 
