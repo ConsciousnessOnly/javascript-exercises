055046bc7018cfc442547c5d341d1346aefd2c18
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-11-01 18:10:46 +0800

    feat: convert temperatures from F to C, vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5/9;
+  const rounder = Math.round(celsius * 10) / 10;
+  return rounder;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * 9/5 + 32;
+  const rounder = Math.round(fahrenheit * 10) / 10;
+  return rounder;
 };
 
 // Do not edit below this line
