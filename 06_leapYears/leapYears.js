692fb00c6688c611e0bfcf07110aaedb07f20585
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-11-01 16:20:29 +0800

    feat(leapYears): add return condition

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if(Number.isInteger(years) == false) return false;
+    if(years % 400 == 0) return true;
+    if(years % 100 == 0) return false;
+    if(years % 4 == 0) return true;
+    return false;
 };
 
 // Do not edit below this line
