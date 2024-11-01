545c5c89efd72485e81e23f142884ea3f9419601
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-11-01 15:16:52 +0800

    feat(getSum): extract code from sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,36 +1,20 @@
 const sumAll = function (par1, par2) {
-    if (!par1 || !par2) {
-        console.log(`Input empty!`);
-    }
-    else if (Number.isInteger(par1) && Number.isInteger(par2)) {
-        console.log('Integer');
-        if (par1 == par2) {
-            return par1;
-        }
+    if (Number.isInteger(par1) && Number.isInteger(par2)) {
+        if (par1 == par2) return par1;
         else if (par1 > 0 && par2 > 0) {
-            console.log('Larger than 0');
-            let sum = 0;
-            if (par1 > par2) {
-                const minInt = par2;
-                const maxInt = par1;
-
-                for (let int = minInt; int <= maxInt; int++) {
-                    sum += int;
-                }
-            }
-            if (par2 > par1) {
-                const minInt = par1;
-                const maxInt = par2;
-
-                for (let int = minInt; int <= maxInt; int++) {
-                    sum += int;
-                }
-            }
-            return sum;
+            return (par1 > par2) ? getSum(par2, par1) : getSum(par1, par2);
         }
     }
     return "ERROR";
 };
+
+function getSum(minInt, maxInt) {
+    let sum = 0;
+    for (let int = minInt; int <= maxInt; int++) {
+        sum += int;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
