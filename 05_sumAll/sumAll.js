db481f71c0d359dba89ab94afc48533b9bc9fdb0
Shiuan Chen <180915555+ConsciousnessOnly@users.noreply.github.com>
2024-11-01 14:18:22 +0800

    feat(sumAll): sum two parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function (par1, par2) {
+    if (!par1 || !par2) {
+        console.log(`Input empty!`);
+    }
+    else if (Number.isInteger(par1) && Number.isInteger(par2)) {
+        console.log('Integer');
+        if (par1 == par2) {
+            return par1;
+        }
+        else if (par1 > 0 && par2 > 0) {
+            console.log('Larger than 0');
+            let sum = 0;
+            if (par1 > par2) {
+                const minInt = par2;
+                const maxInt = par1;
 
+                for (let int = minInt; int <= maxInt; int++) {
+                    sum += int;
+                }
+            }
+            if (par2 > par1) {
+                const minInt = par1;
+                const maxInt = par2;
+
+                for (let int = minInt; int <= maxInt; int++) {
+                    sum += int;
+                }
+            }
+            return sum;
+        }
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
